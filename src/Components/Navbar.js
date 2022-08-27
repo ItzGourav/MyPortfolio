45806b7df7d1205c8415f293d4569f8a7999e1ed
Gourav <gourav98055@gmail.com>
2022-08-27 19:21:46 +0530

    homeupdate

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -10,14 +10,14 @@ const Navbar = () => {
                 <div className="col-10 mx-auto">
             <nav className="navbar navbar-expand-lg bg-light">
                 <div className="container-fluid">
-                    <NavLink className="navbar-brand" to="/">Gourav</NavLink>
+                    <NavLink className="navbar-brand" to="/Home">Gourav</NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <NavLink exact className="nav-link links" aria-current="page" to="/">Home</NavLink>
+                                <NavLink exact className="nav-link links" aria-current="page" to="/Home">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink exact className="nav-link links" to="/About">About</NavLink>
